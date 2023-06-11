987bfafab8ce16458c53c1872a2bac9cfdd6f719
Oğuz Evrensel <oevrensel@gmail.com>
2023-06-11 16:05:43 +0300

    Added Services and Managers

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfTestimonialDal.cs
@@ -0,0 +1,23 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfTestimonialDal : GenericRepository<Testimonial>, ITestimonialDal
+    {
+
+        public EfTestimonialDal(Context context) : base(context) 
+        {
+                
+        }
+
+    }
+}
